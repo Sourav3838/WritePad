2c623fffc372a23a624e5d38f20397cdac49457f
Sourav3838 <souravsingh10386@gmail.com>
2020-10-09 12:04:14 +0530

    set static folder

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express= require('express')
 const dotenv= require('dotenv')
 //logging
@@ -19,6 +20,8 @@ if(process.env.NODE_ENV === 'development'){
 // Handlebars
 app.engine('.hbs',exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine','hbs');
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 //Routes
 app.use('/',require('./routes/index'))
