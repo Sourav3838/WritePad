2a1c0d31e756b41f90c8da580be792d373755e78
Sourav3838 <souravsingh10386@gmail.com>
2020-10-10 08:47:15 +0530

    Added passport serialization/deserialization

--- config/passport.js
+++ config/passport.js
@@ -14,5 +14,12 @@ module.exports = function (passport) {
 				console.log(profile);
 			}
 		)
-	);
+    );
+    passport.serializeUser((user, done) => {
+		done(null, user.id);
+	});
+
+	passport.deserializeUser((id, done) => {
+		User.findById(id, (err, user) => done(err, user));
+	});
 };
