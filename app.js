ff6bc437fb41e845ddb09c20e876a280dce0f8a6
Sourav3838 <souravsingh10386@gmail.com>
2020-10-10 08:36:57 +0530

    added passport and express-session middleware

--- app.js
+++ app.js
@@ -3,11 +3,18 @@ const express= require('express')
 const dotenv= require('dotenv')
 //logging
 const morgan =require('morgan')
+
+
+const session = require('express-session');
+const passport = require('passport');
+
 //templates
 const exphbs =require('express-handlebars')                                             
 const connectDB = require('./config/db')
 //load config
 dotenv.config({path: './config/config.env'})
+
+require('./config/passport')(passport); //passed the passport const to config/passport.js so that i can use it there
 connectDB()
 const app = express()
 
@@ -20,6 +27,20 @@ if(process.env.NODE_ENV === 'development'){
 // Handlebars
 app.engine('.hbs',exphbs({defaultLayout: 'main', extname: '.hbs'}));
 app.set('view engine','hbs');
+
+
+app.use(
+	session({
+		secret: 'keyboard cat', //it can be anything
+		resave: false, //do not resave the session if nothing is changed
+		saveUninitialized: false, //do not create a session until nothing is stored in it
+	})
+);
+
+//passport middleware
+app.use(passport.initialize());
+app.use(passport.session()); //to work with passport sessions we need express-session
+
 
 app.use(express.static(path.join(__dirname, 'public')))
 
