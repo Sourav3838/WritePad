992111bef7c294e6eb534655c3c825044c82bcb7
Sourav3838 <souravsingh10386@gmail.com>
2020-10-14 13:17:03 +0530

    added route to add stories

--- app.js
+++ app.js
@@ -54,6 +54,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 //Routes
 app.use('/',require('./routes/index'))
 app.use('/auth', require('./routes/auth'));
+app.use('/stories', require('./routes/stories'));
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
